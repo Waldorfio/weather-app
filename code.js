7dfb270718d42793264f004319e50e4c270ff9dd
Waldorfio <walid.hanifi707@gmail.com>
2022-08-31 17:47:06 +1000

    Add weather descriptor and flags

--- code.js
+++ code.js
@@ -21,6 +21,7 @@ async function callWeather(city) {
             try {
                 processObject(response);
                 console.log('City located: '+city);
+                console.log(response);
             } catch (e) {
                 console.log('Error: City not found.')
             }
@@ -28,13 +29,12 @@ async function callWeather(city) {
 }
 
 function processObject(response) {
-    console.log(response);
     city = response.name;
     country = response.sys.country;
     temp = response.main.temp;
     feelsLike = response.main.feels_like;
     humidity = response.main.humidity;
-    weather = response.main.weather;
+    weather = response.weather[0].description.toUpperCase();
     timezone_shift = (response.timezone) - client_timezone;     // Calculating the timezone shift in seconds, relative to the clients own timezone
     sunrise = new Date((response.sys.sunrise + timezone_shift) * 1000).toLocaleTimeString();
     sunset = new Date((response.sys.sunset + timezone_shift) * 1000).toLocaleTimeString();
@@ -44,15 +44,27 @@ function processObject(response) {
 }
 
 function updateDOM(city,country,temp,feelsLike,weather,humidity,sunrise,sunset) {
-    document.getElementById('city').innerHTML = city + ' ' + country;
+    document.getElementById('city').innerHTML = city + ', ' + country;
     document.getElementById('temperature').innerHTML = Math.round(temp* 10)/10 + '<sup>o</sup>C';
     feelsLike = Math.round(feelsLike) + '<sup>o</sup>C';
-    document.getElementById('detail-container').innerHTML = 
-    'Feels like '+feelsLike+' | '+weather+' | Humidity: '+humidity+'% | Sunrise: '+sunrise+' | Sunset: '+sunset;
+    document.getElementById('detail1').innerHTML = 
+    'Feels like '+feelsLike+' | <i>'+weather+'</i> | Humidity: '+humidity+'%';
+    document.getElementById('detail2').innerHTML = 
+    'Sunrise: '+sunrise+' | Sunset: '+sunset;
+
+    document.getElementById('city-flag').src = getCountryCode(country);
 }
 
 // Listens to changes in the search bar, to fire the async function and update the DOM
 searchCity = document.getElementById('search-bar');
 searchCity.addEventListener('input', callWeather.bind(searchCity));
+
+
+// Automatically fetching flags based on country
+function getCountryCode(country) {
+    link = 'https://flagicons.lipis.dev/flags/4x3/'+country.toLowerCase()+'.svg';
+    return link
+}
+
 
 // callWeather('Brisbane');        // Call and display Brisbanes weather by default
